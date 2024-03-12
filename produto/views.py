a0d120dafebd5428d5c698224e4d3003d2e450cb
romario lima <romariolima1006@gmail.com>
2024-03-12 14:56:30 -0300

    detalhe e adicionaraocarrinho pat1

--- produto/views.py
+++ produto/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 
 from . import models
 
@@ -13,12 +15,30 @@ class ListaProduto(ListView):
     paginate_by = 9
 
 
-class DetalheProduto(View):
-    ...
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
-    ...
+    def get(self, *args, **kwargs):
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            reverse('produto:lista')
+        )
+
+        variacao_id = self.request.GET.get('vid')
+
+        if not variacao_id:
+            messages.error(
+                self.request,
+                'Produto não existe'
+            )
+            return redirect(http_referer)
+
+        #return redirect(http_referer)
 
 
 class RemoverDoCarrinho(View):
